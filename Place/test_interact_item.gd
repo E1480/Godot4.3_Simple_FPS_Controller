3d876b44ee6df7a525f11698d51745197112d1cb
E1480 <113873052+E1480@users.noreply.github.com>
2025-02-13 02:56:27 +0000

    Delete

--- Place/test_interact_item.gd
+++
@@ -1,7 +0,0 @@
-extends Interactable
-
-var time = Time.get_time_dict_from_system()
-
-
-func _on_interacted(body:Variant) -> void:
-	print( "[%02d:%02d:%02d]::" % [time.hour, time.minute, time.second] ,"<",body.name,">", " Interacted With ","<", self.name,">")
